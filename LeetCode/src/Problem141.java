ba123ad9e14d64f8dfb9c5295a28510b2b4a2bce
yuruiyin <yuruiyin@cyou-inc.com>
2019-09-28 23:41:28 +0800

    提交反转链表和环形链表两道题的代码

---
+++ LeetCode/src/Problem141.java
@@ -0,0 +1,41 @@
+public class Problem141 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+//        head.next.next.next.next = head.next;
+
+        System.out.println(new Problem141().hasCycle(head));
+    }
+
+}
