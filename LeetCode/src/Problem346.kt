596632647aeee64944d45fbca3a63785409706bc
yuruiyin <yuruiyin@cyou-inc.com>
2019-09-29 20:31:00 +0800

    提交两道leetCode上面关于队列的题

---
+++ LeetCode/src/Problem346.kt
@@ -0,0 +1,28 @@
+import java.util.*
+
+/**
+    给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算其所有整数的移动平均值。
+ */
+class Problem346(val size: Int) {
+
+    private val queue = LinkedList<Int>()
+
+    fun next(`val`: Int): Double {
+        if (queue.size == size) {
+            queue.pollLast()
+        }
+
+        queue.push(`val`)
+
+        return queue.sum() * 1.0 / queue.size
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val m = Problem346(3)
+    println(m.next(1))
+    println(m.next(10))
+    println(m.next(3))
+    println(m.next(5))
+}
