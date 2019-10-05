7d6a6ddf94e6d49f00c03e0d4cb491346efdfbd0
yuruiyin <yuruiyin@cyou-inc.com>
2019-10-05 11:24:00 +0800

    完成一场leetCode上的微软算法题测试

---
+++ LeetCode/src/Problem153.java
@@ -0,0 +1,28 @@
+public class Problem153 {
+
+    public int findMin(int[] nums) {
+        // 二分
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (left == right) {
+                return nums[left];
+            }
+            int mid = (left + right) >>> 1;
+            if (nums[right] > nums[mid]) {
+                // 最小值在左边区域
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Problem153().findMin(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(new Problem153().findMin(new int[]{4,5,6,7,0,1,2}));
+    }
+
+}
---
+++ LeetCode/src/interview_microsoft/round01/Problem01.java
@@ -0,0 +1,59 @@
+package interview_microsoft.round01;
+
+public class Problem01 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    private double[] getClosestValue(TreeNode root, double target, double min) {
+        double[] res = new double[2];
+        int closestValue = root.val;
+        double diff = Math.abs(root.val - target);
+
+        if (diff < min) {
+            min = diff;
+        }
+
+        if (root.left != null) {
+            double[] leftRes = getClosestValue(root.left, target, min);
+            if (leftRes[0] < min) {
+                min = leftRes[0];
+                closestValue = (int) leftRes[1];
+            }
+        }
+
+        if (root.right != null) {
+            double[] rightRes = getClosestValue(root.right, target, min);
+            if (rightRes[0] < min) {
+                min = rightRes[0];
+                closestValue = (int) rightRes[1];
+            }
+        }
+
+        res[0] = min;
+        res[1] = closestValue;
+
+        return res;
+    }
+
+    public int closestValue(TreeNode root, double target) {
+        double[] res = getClosestValue(root, target, Double.MAX_VALUE);
+
+        return (int) res[1];
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        int res = new Problem01().closestValue(root, 3.714286);
+        System.out.println(res);
+    }
+
+}
