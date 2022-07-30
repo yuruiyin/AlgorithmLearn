5680b11d33d488cd4a8445cd864c9741642b07bd
kongran <kongran@gaoding.com>
2022-07-30 11:05:42 +0800

    feat: 完成LC_521题

---
+++ LeetCode/src/problem501_600/Problem521.java
@@ -0,0 +1,9 @@
+package problem501_600;
+
+public class Problem521 {
+
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+    }
+
+}
